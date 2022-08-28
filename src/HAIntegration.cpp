f7b0bb43b184557d73b4800a931ec33fe20af2d9
Danilo Campos <mail@danilocampos.com>
2022-08-27 21:13:02 -0400

    Working MQTT integration

--- src/HAIntegration.cpp
+++ src/HAIntegration.cpp
@@ -1,11 +1,55 @@
 #include "HAIntegration.h"
 #include "Credentials.h"
 
+#include <ArduinoHA.h>
+#include <WiFi.h>
+
+//Adapted via:
+//  https://github.com/dawidchyrzynski/arduino-home-assistant/blob/main/examples/nano33iot/nano33iot.ino
+
+#define LED_PIN     LED_BUILTIN
+
+WiFiClient client;
+HADevice device;
+HAMqtt mqtt(client, device);
+HASwitch led("led", false);
 
 void HAIntegration::configure() {
 
+    //Prepare LED:
+
+    pinMode(LED_PIN, OUTPUT);
+    digitalWrite(LED_PIN, LOW);    
+    
+    //Set device ID as MAC address
+
+    byte mac[WL_MAC_ADDR_LENGTH];
+    WiFi.macAddress(mac);
+    device.setUniqueId(mac, sizeof(mac));
+
+    //Device metadata:
+
+    device.setName("Pico W HA Starter");
+    device.setSoftwareVersion("0.1");
+
+    // handle switch state
+    led.onStateChanged(switchHandler);
+    led.setName("Board LED"); // optional
+
+    Serial.print("Connecting to MQTT\n");
+    
+    if (mqtt.begin(MQTT_BROKER, MQTT_LOGIN, MQTT_PASSWORD) == true) {
+        Serial.print("Connected to MQTT broker");
+    } else {
+        Serial.print("Could not connect to MQTT broker");
+    }
 }
 
-void HAIntegration::loop() {
+void HAIntegration::switchHandler(bool state, HASwitch* sender) {
+    digitalWrite(LED_PIN, (state ? HIGH : LOW));
+}
 
+
+void HAIntegration::loop() {
+    mqtt.loop();
 }
--- src/HAIntegration.h
+++ src/HAIntegration.h
@@ -1,10 +1,13 @@
 #ifndef SRC_HAINTEGRATION
 #define SRC_HAINTEGRATION
 
+#include <ArduinoHA.h>
+
 class HAIntegration {
     public:
     void loop();
     void configure();
+    static void switchHandler(bool state, HASwitch* sender);
 };
 
 #endif // SRC_HAINTEGRATION
